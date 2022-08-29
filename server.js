16838f80d4c8f04ed0225220ebefbe211e51f54c
Lujain92 <lujainaljarrah92@gmail.com>
2022-08-29 22:07:39 +0300

    aa

--- server.js
+++ server.js
@@ -1,2 +1,40 @@
+require('dotenv').config()
 const express = require('express')
-const data =require('./data/ weather.json')
+const dataWeather =require('./data/ weather.json')
+const cors =require('cors')
+const server=express()
+
+const PORT=process.env.PORT;
+
+server.use(cors())
+//http:localhost:3000/weather?lat=latvalue & lon=lonvalue & searchQuery=cityname
+
+server.get('/weather',(req,res)=>{
+    console.log(req.query)
+    // const result =dataWeather.map(item=>{
+        
+    //     // item.city_name
+    //     // item.lat
+    //     // item.lon
+
+    // }
+    //     )
+}
+
+)
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT,()=>{
+    console.log(`hello on ${PORT}`)
+}
+)
+
